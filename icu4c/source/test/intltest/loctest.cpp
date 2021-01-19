fcce137c7eb196731d2ffecf97913a7fc4b21643
Victor Chang <vichang@google.com>
2021-01-19 10:16:37 +0000

    Android patch: CLDR data: Replace nb with no.
    This change was introduced in Android by the ICU51.1 upgrade:
    https://android.googlesource.com/platform/external/icu/+/8393335
    
    And then amended for ICU 57:
    https://android.googlesource.com/platform/external/icu/+/7dbcff1
    
    And then amended for ICU 68:
    Amend the test data in cloctst.c
    
    These changes have been proposed, but not yet made, to CLDR:
    https://unicode.org/cldr/trac/ticket/2698
    
    Test: n/a
    Change-Id: I2dfd989683d013c18c89e6469da2ef28f4e41d4a

--- icu4c/source/test/intltest/loctest.cpp
+++ icu4c/source/test/intltest/loctest.cpp
@@ -2892,8 +2892,8 @@ LocaleTest::TestAddLikelyAndMinimizeSubtags() {
             "nl"
         }, {
             "und_NO",
-            "nb_Latn_NO",
-            "nb"
+            "no_Latn_NO",
+            "no"
         }, {
             "und_NP",
             "ne_Deva_NP",
@@ -3004,8 +3004,8 @@ LocaleTest::TestAddLikelyAndMinimizeSubtags() {
             "sl"
         }, {
             "und_SJ",
-            "nb_Latn_SJ",
-            "nb_SJ"
+            "no_Latn_SJ",
+            "no_SJ"
         }, {
             "und_SK",
             "sk_Latn_SK",
