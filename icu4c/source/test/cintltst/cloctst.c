1215de969174828132d2dd234d180616c6aec21c
Victor Chang <vichang@google.com>
2023-06-29 14:54:17 +0100

    Android patch: Skip cintltst.tsutil.cloctst.TestISOFunction
    Bug: 289028952
    Test: atest CtsIcu4cTestCases
    Change-Id: I9597aace711a5626249550cef71fa896605a2e80

--- icu4c/source/test/cintltst/cloctst.c
+++ icu4c/source/test/cintltst/cloctst.c
@@ -1399,6 +1399,11 @@ static void TestIllegalArgumentWhenNoDataWithNoSubstitute(void)
 /* test for uloc_getISOLanguages, uloc_getISOCountries */
 static void TestISOFunctions(void)
 {
+    // Android-changed: Skip this test on Android because we allow extra languages added on devices.
+    if (true) {
+        return;
+    }
+
     const char* const* str=uloc_getISOLanguages();
     const char* const* str1=uloc_getISOCountries();
     const char* test;
